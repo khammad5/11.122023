7f9c22553698dabb6202e3423da9020aa734ff74
khammad5 <khammadakh@code.edu.az>
2023-11-12 09:25:50 +0400

    Add project files.

---
+++ 11.122023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _11._122023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Muellim videolara axira kimi baxa bilmemisem, 1-2 gune baxib kodlari yazacam");
+        }
+    }
+}
